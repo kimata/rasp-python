d9eb275c71c55f43a70243d9ebca03282374e38c
KIMATA Tetsuya <kimata@green-rabbit.net>
2018-06-25 23:18:37 +0900

    fix code.

--- app/sense_power/sense_power.py
+++ app/sense_power/sense_power.py
@@ -9,6 +9,8 @@ import sys
 import time
 import json
 import traceback
+import logging
+import logging.handlers
 
 sys.path.append(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, 'lib'))
 
@@ -18,6 +20,15 @@ from meter.echonetenergy import get_pan_info
 
 import b_route_config
 
+logger = logging.getLogger("sense_power")
+log_handler = logging.handlers.TimedRotatingFileHandler(
+    '/tmp/sense_power.log', when='D', interval=1, backupCount=3
+)
+log_handler.formatter = logging.Formatter('%(asctime)s %(levelname)s %(name)s :%(message)s')
+log_handler.level = logging.DEBUG
+
+logger.addHandler(log_handler)
+
 echonet_if = BP35A1('/dev/ttyS0', False)
 
 power = 0
@@ -25,7 +36,8 @@ try:
     energy_meter = EchonetEnergy(
         echonet_if,
         b_route_config.b_id,
-        b_route_config.b_pass
+        b_route_config.b_pass,
+        log_handler
     )
 
     pan_info = get_pan_info(energy_meter)
@@ -34,6 +46,7 @@ try:
 except:
     echonet_if.reset()
     echonet_if.reset()
+    logger.error(traceback.format_exc())
     sys.stderr.write(traceback.format_exc())
     exit(-1)
 
