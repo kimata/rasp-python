7a1070e908e65ba2107172b9c630a30a07666a67
KIMATA Tetsuya <kimata@green-rabbit.net>
2019-12-03 22:29:15 +0900

    fix: typo

--- app/watch_wifi/watch_wifi.zsh
+++ app/watch_wifi/watch_wifi.zsh
@@ -5,7 +5,7 @@ COUNT=5
 
 count=$(ping -c $COUNT -q ${GATEWAY_IP} | grep 'received' | awk -F',' '{ print $2 }' | awk '{ print $1 }')
 
-if [ "$count" = "0"]; then
+if [ "$count" = "0" ]; then
     echo "Connectivity test failed. Let's try to reconnect to WiFi AP."
     /sbin/wpa_cli -i wlan0 reconfigure
 else
