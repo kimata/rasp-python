64ab97e04438e6605d924895f8ce576c79219c25
Ubuntu <ubuntu@rasp-meter-1.green-rabbit.net>
2020-06-28 02:02:05 +0900

    fix: 積算電力の収集を止めた

--- app/sense_fplug/sense_fplug.py
+++ app/sense_fplug/sense_fplug.py
@@ -13,10 +13,6 @@ from fplug_list import *
 
 # subprocess.call('sudo rfcomm unbind all', shell=True)
 
-def get_acc_power(fplug):
-    val = fplug.get_acc_power()[-1]
-    return 0 if val is None else val
-
 for i, dev in enumerate(DEVICE_LIST):
     try:
         dev_file = '/dev/rfcomm{0}'.format(i)
@@ -28,12 +24,6 @@ for i, dev in enumerate(DEVICE_LIST):
             'hostname': dev['name'],
             'power': fplug.get_power_realtime(),
             'self_time': 0,
-        }))
-        print(json.dumps({
-            'hostname': dev['name'],
-            'acc_power': get_acc_power(fplug),
-            'self_time': 3600,
-        }))
-        
+        })
     except:
         None
