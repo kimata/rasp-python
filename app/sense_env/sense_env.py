227c6b111bbf563ad66b0b7b3d31741508180893
KIMATA Tetsuya <kimata@green-rabbit.net>
2021-05-08 10:56:55 +0900

    fix: WiFi が 5GHz 接続の場合にエラーになる問題を修正

--- app/sense_env/sense_env.py
+++ app/sense_env/sense_env.py
@@ -85,13 +85,17 @@ sensor_list = detect_sensor()
 value_map = scan_sensor(sensor_list)
 
 wifi_rssi = subprocess.check_output("sudo iwconfig 2>/dev/null | grep 'Signal level' | sed 's/.*Signal level=\\(.*\\) dBm.*/\\1/'", shell=True)
-wifi_rssi = wifi_rssi.rstrip().decode()
+wifi_rssi = int(wifi_rssi.rstrip().decode())
 
 wifi_ch = subprocess.check_output("sudo iwlist wlan0 channel | grep Current | sed -r 's/^.*Channel ([0-9]+)\)/\\1/'", shell=True)
-wifi_ch = wifi_ch.rstrip().decode()
+try:
+    wifi_ch = int(wifi_ch.rstrip().decode())
+except:
+    # 5GHz
+    wifi_ch = 0
 
 if re.compile('-\d+').search(wifi_rssi):
-    value_map['wifi_rssi'] = int(wifi_rssi)
-    value_map['wifi_ch'] = int(wifi_ch)
+    value_map['wifi_rssi'] = wifi_rssi
+    value_map['wifi_ch'] = wifi_ch
 
 print(json.dumps(value_map))
