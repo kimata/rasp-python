c47d75a9bf866a1e58e4fa644e901955bbb9bee3
KIMATA Tetsuya <kimata@green-rabbit.net>
2021-05-20 20:16:24 +0900

    fix: 電源投入後の待ち時間を減らした

--- lib/dev/ltc2874.py
+++ lib/dev/ltc2874.py
@@ -109,7 +109,7 @@ def com_start(spi):
     if enl1 != 0x11:
         # Power on, CQ OC Timeout = 480us
         ltc2874_reg_write(spi, 0x0E, 0x11)
-        time.sleep(5)
+        time.sleep(2)
 
     # Wakeup
     ltc2874_reg_write(spi, 0x0D, 0x10)
